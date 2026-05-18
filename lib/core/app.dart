bce96c56c41390b2838b87bd675897a3e73f2d2f
Jair Conislla <jacobo_9410@hotmail.com>
2026-05-18 04:10:38 -0500

    feat(app): wire AuthBloc and router in CuySentinelApp

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -1,18 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
+import '../feature/auth/application/sign_in_use_case.dart';
+import '../feature/auth/application/sign_out_use_case.dart';
+import '../feature/auth/application/watch_session_use_case.dart';
+import '../presentation/auth/bloc/auth_bloc.dart';
+import 'injection/app_dependencies.dart';
 import 'navigation/app_router.dart';
 import 'theme/app_theme.dart';
 
-class CuySentinelApp extends StatelessWidget {
-  const CuySentinelApp({super.key});
+class CuySentinelApp extends StatefulWidget {
+  const CuySentinelApp({super.key, required this.dependencies});
+
+  final AppDependencies dependencies;
+
+  @override
+  State<CuySentinelApp> createState() => _CuySentinelAppState();
+}
+
+class _CuySentinelAppState extends State<CuySentinelApp> {
+  late final AuthBloc _authBloc;
+  late final GoRouter _router;
+
+  @override
+  void initState() {
+    super.initState();
+    final repo = widget.dependencies.authRepository;
+    _authBloc = AuthBloc(
+      signIn: SignInUseCase(repo),
+      signOut: SignOutUseCase(repo),
+      watchSession: WatchSessionUseCase(repo),
+      initialSession: repo.currentSession(),
+    )..add(const AuthStarted());
+    _router = createAppRouter(_authBloc);
+  }
+
+  @override
+  void dispose() {
+    _authBloc.close();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Cuy Sentinel',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.dark(),
-      routerConfig: appRouter,
+    return BlocProvider.value(
+      value: _authBloc,
+      child: MaterialApp.router(
+        title: 'Cuy Sentinel',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.dark(),
+        routerConfig: _router,
+      ),
     );
   }
 }

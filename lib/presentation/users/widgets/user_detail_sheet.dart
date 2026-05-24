cead5191f958bc645885c9b1df624bec6cfb1967
Jair Conislla <jacobo_9410@hotmail.com>
2026-05-23 19:34:50 -0500

    feat: make user detail dialog reactive

--- lib/presentation/users/widgets/user_detail_sheet.dart
+++ lib/presentation/users/widgets/user_detail_sheet.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../core/responsive/app_breakpoints.dart';
 import '../../../feature/users/domain/entities/panel_user.dart';
 import '../bloc/users_bloc.dart';
+import '../bloc/users_state.dart';
 import '../user_model.dart';
 import 'user_detail_card.dart';
 
@@ -29,13 +30,17 @@ void showUserDetailSheet({
       useSafeArea: true,
       backgroundColor: Colors.transparent,
       barrierColor: Colors.black54,
-      builder: (_) => UserDetailCard(
-        model: model,
-        currentUserRole: currentUserRole,
-        currentUserId: currentUserId,
-        isBottomSheet: true,
-        onPromoteToAdmin: promoteToAdmin,
-        onDemoteToViewer: demoteToViewer,
+      builder: (_) => BlocProvider.value(
+        value: bloc,
+        child: _ReactiveUserDetailCard(
+          initialModel: model,
+          targetUserId: model.userId,
+          currentUserRole: currentUserRole,
+          currentUserId: currentUserId,
+          isBottomSheet: true,
+          onPromoteToAdmin: promoteToAdmin,
+          onDemoteToViewer: demoteToViewer,
+        ),
       ),
     );
   } else {
@@ -43,20 +48,84 @@ void showUserDetailSheet({
       context: context,
       barrierDismissible: true,
       barrierColor: Colors.black54,
-      builder: (_) => Dialog(
-        backgroundColor: Colors.transparent,
-        child: ConstrainedBox(
-          constraints: const BoxConstraints(maxWidth: 420),
-          child: UserDetailCard(
-            model: model,
-            currentUserRole: currentUserRole,
-            currentUserId: currentUserId,
-            isBottomSheet: false,
-            onPromoteToAdmin: promoteToAdmin,
-            onDemoteToViewer: demoteToViewer,
+      builder: (_) => BlocProvider.value(
+        value: bloc,
+        child: Dialog(
+          backgroundColor: Colors.transparent,
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 420),
+            child: _ReactiveUserDetailCard(
+              initialModel: model,
+              targetUserId: model.userId,
+              currentUserRole: currentUserRole,
+              currentUserId: currentUserId,
+              isBottomSheet: false,
+              onPromoteToAdmin: promoteToAdmin,
+              onDemoteToViewer: demoteToViewer,
+            ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class _ReactiveUserDetailCard extends StatelessWidget {
+  const _ReactiveUserDetailCard({
+    required this.initialModel,
+    required this.targetUserId,
+    required this.currentUserRole,
+    required this.currentUserId,
+    required this.isBottomSheet,
+    this.onPromoteToAdmin,
+    this.onDemoteToViewer,
+  });
+
+  final UserModel initialModel;
+  final String targetUserId;
+  final UserRole currentUserRole;
+  final String currentUserId;
+  final bool isBottomSheet;
+  final VoidCallback? onPromoteToAdmin;
+  final VoidCallback? onDemoteToViewer;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<UsersBloc, UsersState>(
+      builder: (context, state) {
+        final (:model, :role) = switch (state) {
+          UsersLoaded() => _resolveLiveModel(state),
+          _ => (model: initialModel, role: currentUserRole),
+        };
+
+        return UserDetailCard(
+          model: model,
+          currentUserRole: role,
+          currentUserId: currentUserId,
+          isBottomSheet: isBottomSheet,
+          onPromoteToAdmin: onPromoteToAdmin,
+          onDemoteToViewer: onDemoteToViewer,
+        );
+      },
+    );
+  }
+
+  ({UserModel model, UserRole role}) _resolveLiveModel(UsersLoaded state) {
+    final targetIndex = state.users.indexWhere((u) => u.id == targetUserId);
+    final targetUser = targetIndex == -1 ? null : state.users[targetIndex];
+    final currentUser = state.users
+        .where((u) => u.id == currentUserId)
+        .firstOrNull;
+
+    return (
+      model: targetUser == null
+          ? initialModel
+          : targetUser.toModel(
+              targetIndex,
+              allLogs: state.accessLogs,
+              presences: state.presences,
+            ),
+      role: currentUser?.role ?? currentUserRole,
     );
   }
 }
